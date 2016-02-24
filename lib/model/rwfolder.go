2ca2a1f048a5e202dbb515160a35e4945532e3b3
Michael Ploujnikov <ploujj@gmail.com>
2016-02-24 09:34:41 -0500

    turn off the scan timer if fsWatcher started successfully only after the initial scan

--- lib/model/rwfolder.go
+++ lib/model/rwfolder.go
@@ -156,7 +156,6 @@ func (p *rwFolder) Serve() {
 	if err != nil {
 		l.Warnln("Unable to setup real-time file change detection")
 	} else {
-		p.delayFullScanForever()
 		go fsWatcher.WaitForEvents()
 	}
 
@@ -297,6 +296,9 @@ func (p *rwFolder) Serve() {
 			if !initialScanCompleted {
 				l.Infoln("Completed initial scan (rw) of folder", p.folder)
 				initialScanCompleted = true
+				if fsWatcher != nil {
+					p.delayFullScanForever()
+				}
 			}
 
 		case req := <-p.scanNow:
